78bcac3ed0ab55daad04731c73ac738e9756a7c6
Zhdanov Yevhen <jeka7753191@gmail.com>
2021-04-15 13:43:13 +0300

    add change task-05

--- js/task-05.js
+++ js/task-05.js
@@ -8,6 +8,19 @@ inputTextElement.addEventListener('input', () => {
         outputTextElement.textContent = inputTextElement.value;
 });
 
+// Вариант № 2 
+// const refs = {
+//     inputNameText: document.querySelector('#name-input'),
+//     spanNameText: document.querySelector('#name-output'),
+// };
+
+// refs.inputNameText.addEventListener("input", onInputChange);
+
+// function onInputChange(event) {
+//     console.log(event.currentTarget.value);
+//     refs.spanNameText.textContent = event.currentTarget.value;
+// };
+
 // Напиши скрипт который, при наборе текста в инпуте input#name - input(событие input), подставляет его текущее значение в span#name - output.Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
 // < input type = "text" placeholder = "Ваше имя?" id = "name-input" />
